82a67aef9e259cddb5dc29bd0c7e965f0859a1f4
window1 <doserack@gmail.com>
2023-02-24 19:07:38 +0900

    Initial Commit

---
+++ ImageDownloader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageDownloader
+//
+//  Created by ChangwonKim on 2023/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
